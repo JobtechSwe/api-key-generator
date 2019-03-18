3043b336644e1fff878ac6cd050f65ef187e37d4
Markus Sköld <markus.skold@arbetsformedlingen.se>
2019-03-18 12:14:26 +0100

    Use env-variable for HOST_URL

--- apikeys/tool.py
+++ apikeys/tool.py
@@ -1,21 +1,32 @@
-import smtplib, ssl
+import smtplib
+import ssl
+import os
+import sys
+import logging
 from apikeys import settings
 from apikeys.repository import postgres
 
 
+log = logging.getLogger(__name__)
+
+
 def send_link_email(recipient, key):
+    host = os.getenv('HOST_URL')
+    if not host:
+        log.error("No HOST_URL environment variable specified")
+        sys.exit(1)
     context = ssl.create_default_context()
     server = smtplib.SMTP(settings.MAIL_HOST, settings.MAIL_PORT)
     server.ehlo()
     server.starttls(context=context)
     server.ehlo()
     server.login(settings.MAIL_USERNAME, settings.MAIL_PASSWORD)
-    message = "Subject: Din API-nyckel hos JobtechDev\n\n" + \
-              "Ladda ner din API-nyckel: http://localhost:5000/key/{key}"
+    message = "Subject: Your API key for JobtechDev\n\n" + \
+              "Get your API key here: {host}/key/{key}"
 
-    server.sendmail(settings.MAIL_SENDER, recipient, message.format(key=key))
+    server.sendmail(settings.MAIL_SENDER, recipient, message.format(host=host, key=key))
 
-    print(f"Sending email to {recipient} containing link http://localhost:5000/key/{key}")
+    print(f"Sending email to {recipient} containing link {host}/key/{key}")
     return True
 
 
