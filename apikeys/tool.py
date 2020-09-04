114eccaa4ec4267597e2afc023ce86d6205e7af3
perui <ulrika.haggqvist@arbetsformedlingen.se>
2020-09-04 15:48:40 +0200

    change text in key mail

--- apikeys/tool.py
+++ apikeys/tool.py
@@ -31,7 +31,7 @@ def send_link_email(recipient, key):
                   " employees and employers to find each other.\n" + \
                   "By following the link You will get Your API key {host}/key/{key}\n" + \
                   "If You have any questions, please send an e-mail to jobtechdev@arbetsformedlingen.se or join our forum at https://jobtechdev.se\n" \
-                  "Thank You and Welcom to the JobTech Community!\n\n" + \
+                  "Thank You and Welcome to the JobTech Community!\n\n" + \
                   "Best regards, The JobTech API teams"
 
         server.sendmail(settings.MAIL_SENDER, recipient,
